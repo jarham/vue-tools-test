63c3fd9a60defda455d802228328e6bf4ce1f76e
Jari Hämäläinen <jari.hamalainen@symcode.fi>
2022-08-26 11:59:49 +0300

    fix: test prev ver assets with patch .9
    Skipped few patch versions by manually pushing .8 tag.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ h3
 .feat
   small {{ t('small-feat') }}
 .feat {{ t('cache-control-test-feat') }}
-.fix.text-secondary {{ t('test-fix', {n: "real .2"}) }}
+.fix.text-secondary {{ t('test-fix', {n: ".9 after skipped ones"}) }}
 </template>
 
 <script setup lang="ts">
