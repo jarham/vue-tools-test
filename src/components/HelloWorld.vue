224e25cd3a9f400a67a0b108f35b1f5e74a497ca
Jari Hämäläinen <jari.hamalainen@symcode.fi>
2022-08-26 12:15:14 +0300

    fix: test prev ver assets with patch .10

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,7 +17,7 @@ h3
 .feat
   small {{ t('small-feat') }}
 .feat {{ t('cache-control-test-feat') }}
-.fix.text-secondary {{ t('test-fix', {n: ".9 after skipped ones"}) }}
+.fix.text-secondary {{ t('test-fix', {n: ".10"}) }}
 </template>
 
 <script setup lang="ts">
